8166b6e8389cc98e1ad82186a225ee40795dce73
ParshakovaIrina <44544412+ParshakovaIrina@users.noreply.github.com>
2019-06-18 00:09:59 +0300

    45
    menu+menu.photo

---
+++ index.js
@@ -0,0 +1,15 @@
+(function($) {
+
+    $(document).ready(function(){
+        var content = $('.menu-content');
+       $('#menu-icon').click(function(){
+           if(content.hasClass("visible")){
+               content.removeClass("visible");
+
+               return;
+           }
+        content.addClass('visible');
+        });
+    });
+
+})(jQuery);
